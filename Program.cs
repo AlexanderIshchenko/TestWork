f79014d58fc0fa59adfcd05ad89872bd0176d7b1
Alexander <063@mail.ru>
2022-08-06 04:18:26 +0400

    Programm

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+int resultArrayLength = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) resultArrayLength++;
+}
+string[] resultArray = new string[resultArrayLength];
+int resultArrayIndex = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resultArray[resultArrayIndex] = array[i];
+        resultArrayIndex++;
+    }
+}
+Console.WriteLine($"[{string.Join(", ", resultArray)}]");
